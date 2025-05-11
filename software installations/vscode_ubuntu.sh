c9550e416bb3b3e4d7e7c254358ec4dbdf47c20c
Sushant Singh <111882164+sushant7326@users.noreply.github.com>
2025-05-11 20:56:02 +0530

    feature: vscode_ubuntu

---
+++ software installations/vscode_ubuntu.sh
@@ -0,0 +1,9 @@
+# had to shift to linux mint because manjaro decided it wont play nice with my hardware anymore
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
